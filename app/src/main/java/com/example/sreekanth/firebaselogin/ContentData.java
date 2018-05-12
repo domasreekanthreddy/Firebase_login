3fdfdd598df4af8a8d6dc4799d6ad1f8b0b599d8
domasreekanthreddy <domasreekanthreddy@gmail.com>
2018-05-12 20:09:56 +0530

    committing source code

---
+++ app/src/main/java/com/example/sreekanth/firebaselogin/ContentData.java
@@ -0,0 +1,24 @@
+package com.example.sreekanth.firebaselogin;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class ContentData extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_content);
+
+
+    }
+
+
+
+}
